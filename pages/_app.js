ea313b18c85b6d5459c4bf8d3da2d530c22b992b
fintoolbox <damianjtoms@gmail.com>
2025-10-24 19:43:11 +1100

    Deploy speed insights Vercel

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
 import "@/styles/globals.css";
 import Layout from "../components/Layout";
 import { Analytics } from "@vercel/analytics/react"; // ✅ add this import
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export default function App({ Component, pageProps }) {
   return (
     <Layout>
       <Component {...pageProps} />
+      <SpeedInsights />
       <Analytics /> {/* ✅ add this just before closing Layout */}
     </Layout>
   );
